6f1e65b74f8b0a03f1b673ebfffa775a4df6173e
hindessc <hindessc@gmail.com>
2024-05-09 19:31:41 +0100

    Added the ability to change the ruleset and read rules from a file, as well as saving rules to a file

--- cellular.py
+++ cellular.py
@@ -1,6 +1,57 @@
 import random
+import sys
 
-WIDTH = 60
+def display(grid):
+    for i in range(HEIGHT):
+        print(convert(grid[i]))
+    print("-"*WIDTH)
+    
+def addRooms(grid, number = 1):
+    rooms = []
+    while len(rooms) < number:
+        i = random.randint(1, HEIGHT-1)
+        j = random.randint(1, WIDTH-1)
+        iDim = random.randint(4, 8)
+        jDim = random.randint(4, 8)
+        skip = False
+        
+        for (i_, iDim_, j_, jDim_) in rooms:
+            if range(max(i, i_), min(i+iDim, i_+iDim_+1)) and range(max(j, j_), min(j+jDim, j_+jDim_+1)):
+                skip = True
+        
+        if not skip:
+            rooms.append((i, iDim, j, jDim))    
+        
+    for (i, iDim, j, jDim) in rooms:
+        for a in range(iDim):
+            print(a)
+            for b in range(jDim):
+                if i+a < HEIGHT-1 and j+b < WIDTH-1:
+                    grid[i+a][j+b] = 1
+                    print(i+a, j+b)
+    return grid
+    
+def getAdjacency(grid, i, j):
+    adjacent = 0
+    for d in adjacencies:
+        a = i + d[0]
+        b = j + d[1]
+        if (0 <= a and a < HEIGHT) and (0 <= b and b < WIDTH):
+            adjacent += grid[a][b]
+    return adjacent
+
+def convert(row):
+    return str(list(map(conversion, row))).replace("[", "").replace("'", "").replace(",", "").replace("]", "")
+
+def conversion(num):
+    match num:
+        case 0:
+            return "█"
+        case 1:
+            return " "
+
+WIDTH = 40
+HEIGHT = 20
 
 adjacencies = [
       [-1, -1]
@@ -13,44 +64,70 @@ adjacencies = [
     , [1, 1]   
     ]
 
-def getAdjacency(grid, i, j):
-    adjacent = 0
-    for d in adjacencies:
-        a = i + d[0]
-        b = j + d[1]
-        try:
-            adjacent += grid[a][b]
-        except IndexError:
-            adjacent += 0
-    return adjacent
-
-def convert(row):
-    return str(list(map(lambda x: "█" if x == 1 else " ", row))).replace("[", "").replace("'", "").replace(",", "").replace("]", "")
-
-curr = []
-for i in range(20):
-    msg = []
-    for j in range(WIDTH):
-        msg.append(1 if random.choice(range(0, 3)) == 2 and i in range(8, 12) and j in range(28, 32) else 0)
-    curr.append(msg)
-
 while True:
-    _ = input()
-    next = []
-    for i in range(20):
+    response = input("Enter either the file name containg the rules, or the rules in the form \"B??/S??\", replacing the question marks with numbers: ")
+    ruleset = ""
+    if response == ":q":
+        sys.exit()
+    elif not response.endswith(".txt"):
+        ruleset = response
+    else:
+        with open(response) as file:
+            ruleset = file.read()
+
+    born = []
+    survive = []
+
+    for rule in ruleset.split("/"):
+        print(rule)
+        match rule[0]:
+            case 'B':
+                born = list(map(int, rule[1:]))
+            case 'S':
+                survive = list(map(int, rule[1:]))
+            case _:
+                raise ValueError("Invalid ruleset")
+
+    curr = []
+    for i in range(HEIGHT):
         msg = []
         for j in range(WIDTH):
-            count = getAdjacency(curr, i, j)
-            if count < 2:
-                msg.append(0)
-            elif (count == 2 or count == 3) and curr[i][j] == 1:
-                msg.append(1)
-            elif count == 3:
-                msg.append(1)
-            else:
-                msg.append(0)
-        next.append(msg)
-    curr = next
-    for i in range(20):
-        print(convert(curr[i]))
-    print("--------------------------------------------")
+            msg.append(1 if random.choice(range(0, 2)) == 1 and i in range(int(HEIGHT/2-2), int(HEIGHT/2+2)) and j in range(int(WIDTH/2-2), int(WIDTH/2+2)) else 0)
+        curr.append(msg)
+
+    while True:
+        match input().split():
+            case [":q"]:
+                break
+            case [":r"]:
+                print(ruleset)
+            case [":w", path]:
+                try:
+                    with open(path.strip(), "x") as file:
+                        file.write(ruleset)
+                except FileExistsError:
+                    match input("File exists, are you sure you want to overwrite it (y/n)"):
+                        case "y":
+                            file.write(ruleset)
+            case [":a", value]:
+                number = int(value)
+                curr = addRooms(curr, number)
+                display(curr) 
+            case _:
+                    
+                next = []
+                for i in range(HEIGHT):
+                    msg = []
+                    for j in range(WIDTH):
+                        count = getAdjacency(curr, i, j)
+                        if i == 0 or i == HEIGHT-1 or j == 0 or j == WIDTH-1:
+                            msg.append(0)
+                        elif count in survive and curr[i][j] == 1:
+                            msg.append(1)
+                        elif count in born:
+                            msg.append(1)
+                        else:
+                            msg.append(0)
+                    next.append(msg)
+                curr = next
+                display(curr)
